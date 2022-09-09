94f37c121418b17fda120a8b49a6dd630b2259ed
JessieLei <alen31002000@gmail.com>
2022-09-09 20:22:33 +0800

    first commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -12,3 +12,34 @@ $(".row")
   .progress(function () {
     $(".row").masonry(); // 渲染整體畫面
   });
+
+// mobile搜尋框展開
+$(".navbar-search").click(function () {
+  $(".search-bar-open").toggleClass("d-none");
+});
+$(".search-bar-back").click(function () {
+  $(".search-bar-open").toggleClass("d-none");
+});
+
+// index-swiper
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 24,
+  loop: true,
+  freeMode: true,
+  autoplay: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      centeredSlides: false,
+      slidesPerView: 1,
+    },
+    992: {
+      centeredSlides: true,
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
+});
